8f610658af5245348ec46549ee02e9dab2c972e6
YLAzY <164717076+cleanper@users.noreply.github.com>
2025-07-29 23:42:22 +0800

    Fix

--- common/src/main/java/org/embeddedt/modernfix/dynamicresources/DynamicModelCache.java
+++ common/src/main/java/org/embeddedt/modernfix/dynamicresources/DynamicModelCache.java
@@ -1,22 +1,24 @@
 package org.embeddedt.modernfix.dynamicresources;
 
 import it.unimi.dsi.fastutil.Function;
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
 import net.minecraft.client.resources.model.BakedModel;
 import java.util.concurrent.locks.StampedLock;
+import java.lang.ref.Cleaner;
 
-public class DynamicModelCache<K> {
+public final class DynamicModelCache<K> {
     private final Reference2ReferenceOpenHashMap<K, BakedModel> cache;
     private final StampedLock lock = new StampedLock();
     private final Function<K, BakedModel> modelRetriever;
     private final boolean allowNulls;
     private static final int MAX_SIZE = 1000;
+    private static final Cleaner CLEANER = Cleaner.create();
 
     public DynamicModelCache(Function<K, BakedModel> modelRetriever, boolean allowNulls) {
         this.modelRetriever = modelRetriever;
         this.allowNulls = allowNulls;
         this.cache = new Reference2ReferenceOpenHashMap<>(Math.min(MAX_SIZE, 512));
+        CLEANER.register(this, new CacheCleaner(cache));
     }
 
     public void clear() {
@@ -49,9 +51,7 @@ public class DynamicModelCache<K> {
                 long writeStamp = lock.writeLock();
                 try {
                     BakedModel existing = cache.get(key);
-                    if (existing != null) {
-                        return existing;
-                    }
+                    if (existing != null) return existing;
 
                     if (cache.size() >= MAX_SIZE) {
                         cache.remove(cache.keySet().iterator().next());
@@ -70,12 +70,18 @@ public class DynamicModelCache<K> {
         return model;
     }
 
-    @Override
-    protected void finalize() throws Throwable {
-        try {
-            clear();
-        } finally {
-            super.finalize();
+    private static final class CacheCleaner implements Runnable {
+        private final Reference2ReferenceOpenHashMap<?, ?> mapToClear;
+
+        CacheCleaner(Reference2ReferenceOpenHashMap<?, ?> map) {
+            this.mapToClear = map;
+        }
+
+        @Override
+        public void run() {
+            synchronized (mapToClear) {
+                mapToClear.clear();
+            }
         }
     }
 }
